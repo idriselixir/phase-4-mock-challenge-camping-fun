649945b9bda72b6ce97c080706e2759bdcd5f371
Idris Noor <idreesnoor150@gmail.com>
2023-06-27 01:12:43 +0300

    done

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,34 @@
+class CampersController < ApplicationController
+    def index
+      campers = Camper.all
+      render json: campers, only: [:id, :name, :age]
+    end
+  
+    def show
+      camper = Camper.find_by(id: params[:id])
+      if camper
+        render json: camper, only: [:id, :name, :age], include: { activities: { only: [:id, :name, :difficulty] } }
+      else
+        render json: { error: 'Camper not found' }, status: :not_found
+      end
+    end
+  
+    def show_with_activities
+      show
+    end
+
+    def create
+        camper = Camper.new(camper_params)
+        if camper.save
+          render json: camper, status: :created
+        else
+          render json: { errors: camper.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    
+      private
+    
+      def camper_params
+        params.permit(:name, :age)
+      end
+    end
